d34559347402fb1d6b1145ab0f8b472f55f0ad93
Realharpi <harpiwarrach@gmail.com>
2022-07-29 19:34:15 +0200

    Added Navbar conponent & Minor adjustments

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { About, Footer, Header, Skills, Testimonial, Work } from './container'
+import { Navbar } from './components'
 
 const App = () => {
   return (
     <div className="App">
       <h1>It's working!</h1>
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
     </div>
   );
 }
